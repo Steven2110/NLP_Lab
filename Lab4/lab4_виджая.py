5dcb643391ed703802025e788d8543e920f23de1
Steven Wijaya <steven.wijaya2001@gmail.com>
2024-02-03 12:10:24 +0700

    edit lab 4 python file

--- Lab4/lab4_виджая.py
+++ Lab4/lab4_виджая.py
@@ -19,7 +19,7 @@ name = 'bert-base-multilingual-uncased'
 tokenizer = BertTokenizer.from_pretrained(name)
 model = BertForMaskedLM.from_pretrained(name, return_dict = True)
 
-text = "Обычно я ходил в" + tokenizer.mask_token + " с друзями каждая пятница."
+text = "My favourite fruit is " + tokenizer.mask_token + ", mandarin, pear and the fruits name that is used as company name by Steve Jobs."
 input =tokenizer.encode_plus(text, return_tensors = "pt")
 mask_index = torch.where(input["input_ids"][0] == tokenizer.mask_token_id)
 output = model(**input)
@@ -28,5 +28,5 @@ logits = output.logits
 softmax = F.softmax(logits, dim = -1)
 mask_word = softmax[0, mask_index[0], :]
 top = torch.topk(mask_word, 10)
-for token in top[-1][0].data:
-    print(tokenizer.decode([token]))
+for i, token in enumerate(top[-1][0].data):
+    print(f"{i + 1}. {tokenizer.decode([token])}")
